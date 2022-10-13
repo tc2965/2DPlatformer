f0993cf50063b1c111ff07008cf98f004ce0b138
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-13 14:59:19 -0400

    Added base service code. This branch will only have code and no scene updates

---
+++ Assets/Code/Base/Bunny/BunnyMessage.cs
@@ -0,0 +1,33 @@
+using Unity.Collections;
+using UnityEngine;
+
+// May have to look to building a policy object for enforcing things like a lifetime policy.
+
+public class BunnyMessage<T> : MonoBehaviour
+{
+
+    [ReadOnly] public BunnyEntity to;
+    [ReadOnly] public BunnyEntity from;
+    public float lifetime;
+    public object _payload;
+
+    public T GetValue<T>()
+    {
+        if(_payload is T) {
+            return (T)_payload;
+        }
+        return default(T);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
