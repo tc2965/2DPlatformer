8a4cb5d0b393dc433bb80f305c1c72c646a413de
tc2965 <tc2965@nyu.edu>
2022-10-20 20:30:19 -0400

    make candy collectible for bullets and shoot

---
+++ Assets/Code/Candy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private float yStart;
+    private Vector3 _position;
+
+    void Start()
+    {
+        yStart = transform.position.y;
+        _position = transform.position;
+    }
+
+    void Update()
+    {
+        _position.y = yStart + 0.5f * Mathf.Sin(speed * Time.fixedTime);
+        transform.position = _position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) {
+            other.gameObject.GetComponent<PlayerCode>().IncrementBullets();
+            Destroy(gameObject);
+        }
+    }
+}
