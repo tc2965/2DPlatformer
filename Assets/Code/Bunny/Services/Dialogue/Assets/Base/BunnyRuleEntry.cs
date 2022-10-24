a67a6bfef46f068bb327da422d05313c07e44c36
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-24 17:19:32 -0400

    Made the base of the Dialogue system but not enough time to polish

--- Assets/Code/Bunny/Services/Dialogue/Assets/Base/BunnyRuleEntry.cs
+++ Assets/Code/Bunny/Services/Dialogue/Assets/Base/BunnyRuleEntry.cs
@@ -9,20 +9,78 @@ public enum BunnyRulePriority
     High
 }
 
+// Core building block of dialogue
+// When an event is dispatched, all rules listening to it are gathered 1 by 1
+// The rule that matches with the highest specificity (most criteria matched) runs
+// Algorithm should have a sorted collection for rules as the algorithm should stop at the first rule found.
+
+// Events are also facts under the hood can be considered within the criterion.
+// Conversations that have several rules will use the same event.
+// They'll have criteria that requires the event entry to be their position in the dialogue sequence.
+
 public class BunnyRuleEntry : BunnyBaseEntry
 {
-    public BunnyEvent TriggeredBy;
-    public BunnyEvent Triggers;
-    public List<BunnyDialogueCriteria> Criteria;
-    private BunnyRulePriority _priority;
+    public BunnyDialogueCriteria[] criterion;
+    public string Key;
+    public bool Once;
+    public bool IsCancellable;
+    public float Padding;
+    public float Delay;
+    public BunnyEventEntry TriggeredBy;
+    public BunnyEventEntry Triggers;
+    public BunnyFactEntry Speaker;
+    public string Text;
 
-    public void Execute()
+    public BunnyRuleEntry(
+        BunnyDialogueCriteria[] criteria,
+        BunnyEventEntry trigger,
+        BunnyFactEntry speaker,
+        string dialogue
+    )
     {
-
+        criterion = criteria;
+        TriggeredBy = trigger;
+        this.Speaker = speaker;
+        Text = dialogue;
     }
 
-    public BunnyRulePriority GetPriority()
+    public BunnyRuleEntry(
+        BunnyDialogueCriteria[] criteria,
+        BunnyEventEntry trigger,
+        BunnyFactEntry speaker,
+        string dialogue,
+        BunnyEventEntry next
+    )
     {
-        return _priority;
+        criterion = criteria;
+        TriggeredBy = trigger;
+        this.Speaker = speaker;
+        Text = dialogue;
+        Triggers = next;
+    }
+
+    public bool Validate()
+    {
+        for(var i=0; i < criterion.Length; i++)
+        {
+            if(!criterion[i].IsSatisfied()) 
+                return false;
+        }
+        return true;
+    }
+
+    // Int passed will be event entry ID
+    public void Execute(BunnyBrokerMessage<int> message)
+    {
+        for(var i=0; i < criterion.Length; i++)
+        {
+            if(!criterion[i].IsSatisfied()) 
+                return;  
+        }
+    }
+
+    public int GetPriority()
+    {
+        return criterion.Length;
     }
 }
