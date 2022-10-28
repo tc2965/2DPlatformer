859de6436bddc5b992b2ff33cbb6773ea58c6e25
tc2965 <tc2965@nyu.edu>
2022-10-27 22:04:13 -0400

    boss anims

--- Assets/Code/ToothBoss.cs
+++ Assets/Code/ToothBoss.cs
@@ -12,6 +12,7 @@ public class ToothBoss : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        StartCoroutine(goThroughAnims());
     }
 
     // Update is called once per frame
@@ -19,5 +20,19 @@ public class ToothBoss : MonoBehaviour
     {
         // _rigidbody.velocity = new Vector2(_rigidbody.velocity.x + 0.1, _rigidbody.velocity.y);
         _animator.SetFloat("Speed", Mathf.Abs(0.15f));
+        
+    }
+
+    IEnumerator goThroughAnims() {
+        _animator.SetFloat("Speed", Mathf.Abs(0.15f));
+        yield return new WaitForSeconds(1);
+        _animator.SetFloat("Speed", 0);
+        yield return new WaitForSeconds(1);
+        _animator.SetTrigger("Eat");
+        yield return new WaitForSeconds(1);
+        _animator.SetTrigger("Damaged");
+        yield return new WaitForSeconds(1);
+        _animator.SetTrigger("Rage");
+        yield return new WaitForSeconds(1);
     }
 }
