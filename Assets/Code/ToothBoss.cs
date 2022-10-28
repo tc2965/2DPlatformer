45daa19c74b0ead4419aab764fa2391dea98f685
tc2965 <tc2965@nyu.edu>
2022-10-27 21:24:38 -0400

    walking animation ex

--- Assets/Code/ToothBoss.cs
+++ Assets/Code/ToothBoss.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ToothBoss : MonoBehaviour
 {
+    Rigidbody2D _rigidbody; //in inspector: make gravity scale 5, freeze rotation on z axis
+    Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // _rigidbody.velocity = new Vector2(_rigidbody.velocity.x + 0.1, _rigidbody.velocity.y);
+        _animator.SetFloat("Speed", Mathf.Abs(0.15f));
     }
 }
